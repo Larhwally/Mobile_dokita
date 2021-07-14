756fbcbb6c4d3e0a7a8dbb678aca72d9d72f07f8
Larhwally <lawalwhylawal@gmail.com>
2021-07-15 00:54:24 +0100

    welcome screen coming arounf

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_doctor/components/rounded_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
-  const WelcomeScreen({Key? key}) : super(key: key);
-
+  static const String id = 'welcome_screen';
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
@@ -10,6 +10,59 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 25.0,
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      text: 'DOKITA',
+                      style: TextStyle(color: Colors.indigo.shade400),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                child: Image.asset('images/banner.png'),
+                height: 200.0,
+              ),
+              RichText(
+                text: TextSpan(
+                  text: 'Request for doctors at the ease of your fingertips',
+                  style: TextStyle(color: Colors.lightBlueAccent),
+                ),
+              ),
+              RoundedButton(
+                title: 'Sign in',
+                colour: Colors.deepPurple.shade200,
+                onPressed: () {
+                  //do some operations here
+                },
+              ),
+              RoundedButton(
+                title: 'Sign up as Doctor',
+                colour: Colors.white60,
+                onPressed: () {},
+              ),
+              RoundedButton(
+                title: 'Emergency (SOS)',
+                colour: Colors.red.shade400,
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
